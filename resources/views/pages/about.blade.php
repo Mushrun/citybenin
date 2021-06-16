187fb908c44d3538506bc73e85083fa0d1d9b82f
Mushrun <samduchrist8@gmail.com>
2021-06-16 18:22:41 +0200

    Do bot dispalays footer about link on about page

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,7 +5,7 @@
 @section('content')
         <p> Built with &hearts; by Novotech</p>
 
-        <p><a href="/">Revenir à la page d'accueil</a></p>
+        <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
 
         
 @endsection
