a7aef3e62a2cf5e1c28bd7035e6ca781c0b82fb9
Mushrun <samduchrist8@gmail.com>
2021-06-16 17:21:25 +0200

    Remove duplication by using base layout

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,21 +1,8 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title> About-us | Hello From Benin</title>
-
-      
-    </head>
-    <body >
+@extends('base')
+@section('content')
         <p> Built with &hearts; by Novotech</p>
 
         <p><a href="/">Revenir à la page d'accueil</a></p>
 
-        <footer>
-            <p> &copy; Copyright  {{ date('Y') }}&middot; </p>
-
-        </footer>
-    </body>
-</html>
+        
+@endsection
