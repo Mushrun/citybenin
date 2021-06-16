fbc16ee72b9071ab9125a2b376e0629610449b51
Mushrun <samduchrist8@gmail.com>
2021-06-16 18:33:56 +0200

    Add images

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,6 +3,7 @@
 @section('title', 'About-us | ' . config('app.name'))
 
 @section('content')
+        <img src="/img/mushrun2.jfif" alt="Mushrun Image">
         <p> Built with &hearts; by Novotech</p>
 
         <p><a href="{{ route('home') }}">Revenir à la page d'accueil</a></p>
