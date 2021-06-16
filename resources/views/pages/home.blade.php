ccd93b065574fa219e83d68d06add507f47c08f5
Mushrun <samduchrist8@gmail.com>
2021-06-16 18:51:21 +0200

    Make use of asset function

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,7 +3,7 @@
 @section('title',  config('app.name'))
   
 @section('content')
-        <img src="/img/imagesbenin.png" alt="Benin Flag">
+        <img src="{{ asset('/img/imagesbenin.png') }}" alt="Benin Flag">
         <h1> Hello from Benin</h1>
 
         <p> It's currently {{ date('h:i A') }} </p>
